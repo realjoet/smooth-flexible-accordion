4fee0295fb6e8b2574a125a543ba11288477fff4
Joe Toscano <realjoet9@gmail.com>
2015-05-15 15:16:13 -0600

    updated to allow margin on accordion mastheads

--- smooth-accordion.js
+++ smooth-accordion.js
@@ -1,17 +1,44 @@
 $(function () {
 
     //Checking the position of panels
-    var allPanels = $('.content').hide();
+    var allContentPanels = $('.content')
+    var allMastheads = $('.masthead')
+    var hideContentPanels = allContentPanels.hide();
+    //If you want margin on your mastheads, enter it here just like CSS
+    var mastheadMargin = "0 0 10px";
+    //
+    var marginTopFix = "-10px";
+    var marginRightFix = "0";
+    var marginBottomFix = "0";
+    var marginLeftFix = "0";
+
+    allMastheads.css("margin", mastheadMargin);
 
     //working accordion code -- needs to be smoother
-    $('.accordion > .masthead').click(function (event) {
+    $('.accordion > .logo-wrapper').click(function (event) {
       var contentHeight = $('.active').next().height();
       var topPos = $(this).position();
 
+      if ($(this).hasClass('active')) {
+        $(allMastheads).css("margin", mastheadMargin);
+        $(this).next().css({
+          "position": "relative", 
+          "top": marginTopFix, 
+          "right": marginRightFix, 
+          "bottom": marginBottomFix, 
+          "left": marginLeftFix
+        });
+      } else {
+        $(allMastheads).css("margin", mastheadMargin);
+        $(this).css("margin", "0");
+        $(this).next().css("margin", mastheadMargin);
+      }
+
       if (contentHeight < 400) {
+        console.log('first');
         if ($(this).hasClass('active')) {
           $(this).removeClass('active')
-            .next(allPanels).slideUp(400);
+            .next(allContentPanels).slideUp(400);
           $('body').animate({scrollTop: topPos.top}, 400);
         } else {
           if ($('.active').length && $('.active').index() < $(this).index()) {
@@ -21,14 +48,15 @@ $(function () {
           }
           
           $('.active').removeClass('active')
-            .next(allPanels).slideUp(400);
+            .next(allContentPanels).slideUp(400);
           $(this).addClass('active')
-            .next(allPanels).slideDown(400);
+            .next(allContentPanels).slideDown(400);
         }
       } else if (contentHeight < 800) {
+        console.log('second');
         if ($(this).hasClass('active')) {
           $(this).removeClass('active')
-            .next(allPanels).slideUp(700);
+            .next(allContentPanels).slideUp(700);
           $('body').animate({scrollTop: topPos.top}, 400);
         } else {
           var topPos = $(this).position();
@@ -40,14 +68,15 @@ $(function () {
           }
           
           $('.active').removeClass('active')
-            .next(allPanels).slideUp(800);
+            .next(allContentPanels).slideUp(800);
           $(this).addClass('active')
-            .next(allPanels).slideDown(800);
+            .next(allContentPanels).slideDown(800);
         }
       } else if (contentHeight < 1200) {
+        console.log('third');
         if ($(this).hasClass('active')) {
           $(this).removeClass('active')
-            .next(allPanels).slideUp(1000);
+            .next(allContentPanels).slideUp(1000);
           $('body').animate({scrollTop: topPos.top}, 400);
         } else {
           var topPos = $(this).position();
@@ -59,15 +88,16 @@ $(function () {
           }
           
           $('.active').removeClass('active')
-            .next(allPanels).slideUp(1000);
+            .next(allContentPanels).slideUp(1000);
           $(this).addClass('active')
-            .next(allPanels).slideDown(1000);
+            .next(allContentPanels).slideDown(1000);
         }
       } else {
+        console.log('fourth');
         console.log("Think about putting less content in there, no one wants to read that much anyway");
         if ($(this).hasClass('active')) {
           $(this).removeClass('active')
-            .next(allPanels).slideUp(1200);
+            .next(allContentPanels).slideUp(1200);
           $('body').animate({scrollTop: topPos.top}, 400);
         } else {
           var topPos = $(this).position();
@@ -79,9 +109,9 @@ $(function () {
           }
           
           $('.active').removeClass('active')
-            .next(allPanels).slideUp(1200);
+            .next(allContentPanels).slideUp(1200);
           $(this).addClass('active')
-            .next(allPanels).slideDown(1200);
+            .next(allContentPanels).slideDown(1200);
         }
       }
     });
