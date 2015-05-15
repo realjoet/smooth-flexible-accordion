234b4211a5e6099d1cddf3fb2cdfad31f0108126
Joe Toscano <realjoet9@gmail.com>
2015-05-15 13:53:56 -0600

    updated js to make it smooth across content heights

--- smooth-accordion.js
+++ smooth-accordion.js
@@ -2,11 +2,12 @@ $(function () {
 
     //Checking the position of panels
     var allPanels = $('.content').hide();
-    var mastheadHeight = $('.masthead').height();
+    var contentHeight = $('.content').height();
 
     //working accordion code -- needs to be smoother
     $('.accordion > .masthead').click(function (event) {
 
+      if (contentHeight < 400) {
         if ($(this).hasClass('active')) {
           $(this).removeClass('active')
             .next(allPanels).slideUp(400);
@@ -14,7 +15,7 @@ $(function () {
           var topPos = $(this).position();
           
           if ($('.active').length && $('.active').index() < $(this).index()) {
-            $('body').animate({scrollTop: topPos.top - mastheadHeight}, 600);
+            $('body').animate({scrollTop: topPos.top - contentHeight}, 600);
           } else { 
             $('body').animate({scrollTop: topPos.top}, 600);
           }
@@ -24,6 +25,45 @@ $(function () {
           $(this).addClass('active')
             .next(allPanels).slideDown(400);
         }
+      } else if (contentHeight < 800) {
+        if ($(this).hasClass('active')) {
+          $(this).removeClass('active')
+            .next(allPanels).slideUp(700);
+        } else {
+          var topPos = $(this).position();
+          
+          if ($('.active').length && $('.active').index() < $(this).index()) {
+            $('body').animate({scrollTop: topPos.top - contentHeight}, 1000);
+          } else { 
+            $('body').animate({scrollTop: topPos.top}, 1000);
+          }
+          
+          $('.active').removeClass('active')
+            .next(allPanels).slideUp(700);
+          $(this).addClass('active')
+            .next(allPanels).slideDown(700);
+        }
+      } else if (contentHeight < 1200) {
+        if ($(this).hasClass('active')) {
+          $(this).removeClass('active')
+            .next(allPanels).slideUp(1000);
+        } else {
+          var topPos = $(this).position();
+          
+          if ($('.active').length && $('.active').index() < $(this).index()) {
+            $('body').animate({scrollTop: topPos.top - contentHeight}, 1200);
+          } else { 
+            $('body').animate({scrollTop: topPos.top}, 1200);
+          }
+          
+          $('.active').removeClass('active')
+            .next(allPanels).slideUp(1000);
+          $(this).addClass('active')
+            .next(allPanels).slideDown(1000);
+        }
+      } else {
+        console.log("Think about putting less content in there, no one wants to read that much anyway");
+      }
     });
 
 });
