ebaf866ddd1b93c33712fa2000eab500eb23939e
Joe Toscano <realjoet9@gmail.com>
2015-05-16 07:40:56 -0600

    super clean js

--- smooth-accordion.js
+++ smooth-accordion.js
@@ -1,81 +1,79 @@
 $(function () {
+  //Checking the position of panels
+  var allContentPanels = $('.content')
+  var allMastheads = $('.masthead')
+  var hideContentPanels = allContentPanels.hide();
+  //If you want margin on your mastheads, enter it here just like CSS
+  var mastheadMargin = "0";
+  //Need to fill these in with negative values of the values you put in for mastheadMargin
+  var marginTopFix = "0";
+  var marginRightFix = "0";
+  var marginBottomFix = "0";
+  var marginLeftFix = "0";
 
-    //Checking the position of panels
-    var allContentPanels = $('.content')
-    var allMastheads = $('.masthead')
-    var hideContentPanels = allContentPanels.hide();
-    //If you want margin on your mastheads, enter it here just like CSS
-    var mastheadMargin = "0";
-    //Need to fill these in with negative values of the values you put in for mastheadMargin
-    var marginTopFix = "0";
-    var marginRightFix = "0";
-    var marginBottomFix = "0";
-    var marginLeftFix = "0";
+  allMastheads.css("margin", mastheadMargin);
 
-    allMastheads.css("margin", mastheadMargin);
+  //working accordion code
+  $('.accordion > .masthead').click(function() {
+    var el = $(this);
+    var activeEl = $('.active');
+    var contentHeight = $('.active').next().height();
+    var topPos = $(this).position();
 
-    //working accordion code
-    $('.accordion > .masthead').click(function() {
-      var el = $(this);
-      var activeEl = $('.active');
-      var contentHeight = $('.active').next().height();
-      var topPos = $(this).position();
+    function marginFix(el) {
+      $(el).next().css({
+        "position": "relative", 
+        "top": marginTopFix, 
+        "right": marginRightFix, 
+        "bottom": marginBottomFix, 
+        "left": marginLeftFix
+      }).delay(10);
+    }
 
-      function marginFix(el) {
-        $(el).next().css({
-          "position": "relative", 
-          "top": marginTopFix, 
-          "right": marginRightFix, 
-          "bottom": marginBottomFix, 
-          "left": marginLeftFix
-        }).delay(10);
-      }
-
-      //Adjusts masthead margins
-      if ($(this).hasClass('active')) {
-        marginFix(el);
-        $(allMastheads).css("margin", mastheadMargin);
+    //Accordion function
+    function accordionFunc(el, animationTime, animationTime2) {
+      if (el.hasClass('active')) {
+        el.removeClass('active')
+          .next(allContentPanels).slideUp(animationTime);
+        $('body').animate({scrollTop: topPos.top}, animationTime);
       } else {
-        marginFix(activeEl);
-        $(allMastheads).css("margin", mastheadMargin);
-        $(this).css("margin", "0");
-        $(this).next().css("margin", mastheadMargin);
-        if ($(this).next().css("top") == marginTopFix) {
-          $(this).next().css("top", "0");
+        if (activeEl.length && activeEl.index() < el.index()) {
+          $('body').animate({scrollTop: topPos.top - contentHeight}, animationTime2);
+        } else { 
+          $('body').animate({scrollTop: topPos.top}, animationTime2);
         }
+        
+        activeEl.removeClass('active')
+          .next(allContentPanels).slideUp(animationTime);
+        el.addClass('active')
+          .next(allContentPanels).slideDown(animationTime);
       }
+    }
 
-      //Accordion function
-      function accordionFunc(el, animationTime, animationTime2) {
-        if (el.hasClass('active')) {
-          el.removeClass('active')
-            .next(allContentPanels).slideUp(animationTime);
-          $('body').animate({scrollTop: topPos.top}, animationTime);
-        } else {
-          if ($('.active').length && $('.active').index() < el.index()) {
-            $('body').animate({scrollTop: topPos.top - contentHeight}, animationTime2);
-          } else { 
-            $('body').animate({scrollTop: topPos.top}, animationTime2);
-          }
-          
-          $('.active').removeClass('active')
-            .next(allContentPanels).slideUp(animationTime);
-          el.addClass('active')
-            .next(allContentPanels).slideDown(animationTime);
-        }
+    //Adjusts masthead margins
+    if ($(this).hasClass('active')) {
+      marginFix(el);
+      $(allMastheads).css("margin", mastheadMargin);
+    } else {
+      marginFix(activeEl);
+      $(allMastheads).css("margin", mastheadMargin);
+      $(this).css("margin", "0");
+      $(this).next().css("margin", mastheadMargin);
+      if ($(this).next().css("top") == marginTopFix) {
+        $(this).next().css("top", "0");
       }
+    }
 
-      //Creates content-responsive accordion
-      if (contentHeight < 400) {
-        accordionFunc(el, 600, 800);
-      } else if (contentHeight < 800) {
-        accordionFunc(el, 800, 1000);
-      } else if (contentHeight < 1200) {
-        accordionFunc(el, 1000, 1200);
-      } else {
-        console.log("Think about putting less content in there, no one wants to read that much anyway");
-        accordionFunc(el, 1200, 1400);
-      }
-    });
-
+    //Creates content-responsive accordion
+    if (contentHeight < 400) {
+      accordionFunc(el, 600, 800);
+    } else if (contentHeight < 800) {
+      accordionFunc(el, 800, 1000);
+    } else if (contentHeight < 1200) {
+      accordionFunc(el, 1000, 1200);
+    } else {
+      console.log("Think about putting less content in there, no one wants to read that much anyway");
+      accordionFunc(el, 1200, 1400);
+    }
+  });
 });
